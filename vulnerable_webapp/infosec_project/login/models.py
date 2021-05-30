787da72ddb19888d40fb8eec3122500fbdd6256c
GilbertRoss <bhuiyannazrul74@gmail.com>
2021-05-30 19:06:57 +0200

    dockerized everything

--- vulnerable_webapp/infosec_project/login/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-email = models.CharField(max_length= 100)
-password = models.CharField(max_length= 100)
--- vulnerable_webapp/infosec_project/login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import bleach
-
-# Create your views here.
-
-def loginPage(request):
-    return render(request, 'login/login.html', {'hello': bleach.clean('<script>alert(1);</script>')})
